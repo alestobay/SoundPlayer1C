48070371e961d5d83453577247371908b242106a
alestobay <alesto.bay@gmail.com>
2026-06-30 02:06:10 +0300

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace SoundPlayer1C
+{
+    [ComVisible(true)]
+    [Guid("D93B8B7A-AD13-4B5F-8C1D-1F5F8E9F0101")]
+    [ProgId("SoundPlayer1C.Player")]
+    public class Player
+    {
+        private const int SND_ASYNC = 0x0001;
+        private const int SND_FILENAME = 0x00020000;
+        private const int SND_NODEFAULT = 0x0002;
+
+        [DllImport("winmm.dll", CharSet = CharSet.Unicode)]
+        private static extern bool PlaySound(string pszSound, IntPtr hmod, int fdwSound);
+
+        public bool PlayWav(string fileName)
+        {
+            return PlaySound(fileName, IntPtr.Zero,
+                SND_FILENAME | SND_ASYNC | SND_NODEFAULT);
+        }
+
+        public bool Stop()
+        {
+            return PlaySound(null, IntPtr.Zero, 0);
+        }
+    }
+}
